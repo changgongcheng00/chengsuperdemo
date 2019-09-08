bb3efccbaccbb3c49349f8fd29bf78580eacfd39
changgongcheng00 <798822184@qq.com>
2019-09-08 19:52:45 +0800

    q

--- src/main/java/generator/ReetrantLockDemo.java
+++ src/main/java/generator/ReetrantLockDemo.java
@@ -78,7 +78,7 @@ public class ReetrantLockDemo {
         Arunnable arunnable = new Arunnable(reetrantLockDemo);
         Brunnable brunnable = new Brunnable(reetrantLockDemo);
         threads.submit(arunnable);
-        threads.submit(brunnable);1122111
+        threads.submit(brunnable);11221112
     }
 
 }
